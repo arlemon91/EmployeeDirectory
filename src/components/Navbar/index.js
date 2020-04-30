76d4bc7b44544614d7047ef6974147af0ed96c50
Alyssa <lemryaly@gmail.com>
2020-04-29 20:36:22 -0500

    edited spelling in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <h1> Lemony Employee Directory</h1>
+      <h1> Lemon Employee Directory</h1>
       <ul className="navbar-nav">
         <li className="nav-item">
           <Link
